0943464614224e7f1530246d22bc8e9f8fd731df
brettkgamble@gmail.com <5413CrossFit2018>
2020-11-21 09:05:11 -0700

    0002 - Transitions

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -5,7 +5,7 @@ import { Transition } from "react-transition-group";
 class TransitionComp extends Component{
 
     state = {
-        show: true
+        show: false
     }
 
     showDiv = () => {
@@ -20,9 +20,20 @@ class TransitionComp extends Component{
             <>
                 <Transition
                     in={this.state.show}
-                    timeout={2000}
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit
                 >
-                    { state => <p>{state}</p>}
+                    { state =>
+                        <div style={{
+                            background: 'red',
+                            height: '100px',
+                            transition: 'all 2s ease',
+                            opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+                        }}>
+                            { state }
+                        </div>
+                    }
                 </Transition>
                 <div className="showDiv" onClick={this.showDiv}>
                     Show / Hide
